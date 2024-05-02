dab182c7fac4f2689bc1892804323cd8eda78bcf
gimsangjun <5321913@naver.com>
2024-05-02 17:59:50 +0900

    feat: add 전체아이템 레이아웃

--- src/containers/HeaderContainer.js
+++ src/containers/HeaderContainer.js
@@ -2,6 +2,7 @@ import React from "react";
 import { MainHeader, NotificationHeader, SubHeader } from "../components/Header";
 
 export default function HeaderContainer() {
+  // TODO: 현재 어느 url에 있는지에 따라서 SubHeader아래에 밑줄
   return (
     <>
       <MainHeader />
--- src/pages/ItemsPage.js
+++ src/pages/ItemsPage.js
@@ -1,12 +1,11 @@
 import React from "react";
-import { MainHeader, SubHeader } from "../components/Header";
+import HeaderContainer from "../containers/HeaderContainer";
 import AllItemsContainer from "../containers/AllItemsContainer";
 
 export default function ItemsPage() {
   return (
     <>
-      <MainHeader />
-      <SubHeader />
+      <HeaderContainer />
       <AllItemsContainer />
     </>
   );
