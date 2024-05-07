187ede41d17bfed7bafd650f6924b1166557e360
gimsangjun <5321913@naver.com>
2024-05-07 16:38:10 +0900

    feat: add 모든 아이템 이미지,가격 ,매물, 업데이트 시간 추가

--- src/components/item/ItemsData.js
+++ src/components/item/ItemsData.js
@@ -2,6 +2,8 @@ import React from "react";
 import tw from "twin.macro";
 
 export default function ItemsData({ items }) {
+  const imgUrl = "https://cdn.bdolytics.com/images/items/";
+
   return (
     // TODO 자식을 벗어나는 부분에 대해서는 배경색깔이 바뀌지않음.(아래로 스크롤 하면 보임.)
     <div className="w-full h-full bg-gray-200 mx-auto flex justify-center items-center flex-col my-5">
@@ -23,14 +25,21 @@ export default function ItemsData({ items }) {
           <tbody className="bg-white divide-gray-200">
             {/* items를 매핑하여 각 행을 생성 */}
             {items.map((item) => (
-              <tr key={item.id} className="hover:bg-gray-100 border-b-2 border-gray-100">
-                <DataTd>{item.name}</DataTd>
+              <tr key={item._id} className="hover:bg-gray-100 border-b-2 border-gray-100">
+                <DataTd className="flex gap-3">
+                  <img
+                    src={`${imgUrl}${String(item.id).padStart(8, "0")}.webp`}
+                    alt="Placeholder"
+                    style={{ width: "28px", height: "28px" }}
+                  />
+                  {formatName(item)}
+                </DataTd>
+                <DataTd>{new Intl.NumberFormat().format(item.basePrice)}</DataTd>
+                <DataTd>{new Intl.NumberFormat().format(item.currentStock)}</DataTd>
                 <DataTd>아직없음</DataTd>
                 <DataTd>아직없음</DataTd>
                 <DataTd>아직없음</DataTd>
-                <DataTd>아직없음</DataTd>
-                <DataTd>아직없음</DataTd>
-                <DataTd>아직없음</DataTd>
+                <DataTd>{formatDate(item.updateAt)}</DataTd>
               </tr>
             ))}
           </tbody>
@@ -39,6 +48,28 @@ export default function ItemsData({ items }) {
     </div>
   );
 }
+
+const formatName = (item) => {
+  let name = item.name;
+  if (item.maxEnhance > 0) {
+    if (item.minEnhance === item.maxEnhance) name += ` ${item.maxEnhance}강`;
+    else name += ` ${item.minEnhance}~${item.maxEnhance}강`;
+  }
+  return name;
+};
+
+// TODO: 정리
+const formatDate = (dateString) => {
+  const date = new Date(dateString);
+  const year = date.getFullYear();
+  const month = String(date.getMonth() + 1).padStart(2, "0");
+  const day = String(date.getDate()).padStart(2, "0");
+  const hours = String(date.getHours()).padStart(2, "0");
+  const minutes = String(date.getMinutes()).padStart(2, "0");
+  const seconds = String(date.getSeconds()).padStart(2, "0");
+
+  return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
+};
 
 const DataTh = tw.th`px-6 py-3 text-left text-xs font-medium text-gray-500 bg-gray-100 uppercase tracking-wider`;
 const DataTd = tw.td`px-6 py-4 whitespace-nowrap`;
