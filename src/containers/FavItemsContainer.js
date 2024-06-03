5536abdb70e7d1ef655ba9fe9f9e907421293205
gimsangjun <5321913@naver.com>
2024-06-03 17:55:17 +0900

    refactor: 아이템 즐겨찾기 기능과 가격 알림 기능 fetch부분 똑같게 통일

--- src/containers/FavItemsContainer.js
+++ src/containers/FavItemsContainer.js
@@ -1,24 +1,54 @@
-import React from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import TableHeader from "../components/item/TableHeader";
 import { useSelector } from "react-redux";
 import ItemsTable from "../components/item/ItemsTable";
+import ItemAPI from "../utils/itemAPI";
 
-// TODO: 즐겨찾기한 아이템 전체 새로고침 버튼
-// TODO: 화면작을때 오른쪽으로 넘기면 이상해짐.
 export default function FavItemsContainer() {
-  const { favItems, loading } = useSelector((state) => state.itemFav);
-  const { loading: updating } = useSelector((state) => state.item); // 가격 업데이트
+  const { favItems, loading: favLoading } = useSelector(
+    (state) => state.itemFav,
+  ); // App.js에서 바로 업데이트함.
+  const [items, setItems] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  // 실제 가격 정보 가져오기.
+  const fetchItems = useCallback(async () => {
+    setLoading(true);
+    try {
+      if (favItems.length === 0) {
+        setItems([]);
+        setLoading(false);
+        return;
+      }
+
+      const _items = favItems.map((item) => ({
+        id: item.id,
+        sid: item.sid,
+      }));
+
+      const fetchedItems = await ItemAPI.getItemsByIdandSid(_items);
+      setItems(fetchedItems);
+    } catch (error) {
+      console.error("Error fetching Items for favItems:", error);
+    } finally {
+      setLoading(false);
+    }
+  }, [favItems]);
+
+  useEffect(() => {
+    fetchItems();
+  }, [favItems, fetchItems]);
 
   return (
     <div className="w-1080 mx-auto">
       <TableHeader tableName={"관심 아이템"} />
       <div className="bg-white flex flex-grow justify-center items-center flex-col rounded-lg">
-        {loading || updating ? (
+        {favLoading || loading ? (
           <div className="flex justify-center items-center w-full h-screen">
             <div className="animate-spin rounded-full h-12 w-12 border-t-4 border-b-4 border-blue-500"></div>
           </div>
         ) : (
-          <ItemsTable items={favItems.map((item) => item.stockDetail)} />
+          <ItemsTable items={items} />
         )}
       </div>
     </div>
--- src/containers/PriceAlertContainer.js
+++ src/containers/PriceAlertContainer.js
@@ -5,15 +5,13 @@ import ItemAPI from "../utils/itemAPI";
 import { fetchPriceAlerts } from "../modules/priceAlert";
 import ItemsTable from "../components/item/ItemsTable";
 
-// TODO: 가격알림한 아이템 전체 새로고침 버튼
-// TODO: 화면작을때 오른쪽으로 넘기면 이상해짐.
 export default function PriceAlertContainer() {
   const { priceAlerts, loading: priceAlertLoading } = useSelector(
     (state) => state.priceAlert,
   );
-  const { loading: updating } = useSelector((state) => state.item); // 가격 업데이트
   const { user } = useSelector((state) => state.auth);
   const [items, setItems] = useState([]); // alert 가격 정보들
+  const [loading, setLoading] = useState(false);
   const dispatch = useDispatch();
 
   // 초기데이터 로딩
@@ -25,18 +23,20 @@ export default function PriceAlertContainer() {
 
   // alert에 등록된 가격 정보들 가져오기
   const fetchItems = useCallback(async () => {
+    setLoading(true);
     try {
       if (priceAlerts.length === 0) {
         setItems([]);
+        setLoading(false);
         return;
       }
 
-      const items = priceAlerts.map((alert) => ({
+      const _items = priceAlerts.map((alert) => ({
         id: alert.itemId,
         sid: alert.itemSid,
       }));
 
-      const fetchedItems = await ItemAPI.getItemsByIdandSid(items);
+      const fetchedItems = await ItemAPI.getItemsByIdandSid(_items);
 
       // fechtedItems 각각에 priceAlerts의 priceThreshold 값을 추가
       const itemsWithPriceThreshold = fetchedItems.map((item) => {
@@ -52,6 +52,8 @@ export default function PriceAlertContainer() {
       setItems(itemsWithPriceThreshold);
     } catch (error) {
       console.error("Error fetching items for price alerts:", error);
+    } finally {
+      setLoading(false);
     }
   }, [priceAlerts]);
 
@@ -63,7 +65,7 @@ export default function PriceAlertContainer() {
     <div className="w-1080 mx-auto">
       <TableHeader tableName={"가격 알림"} />
       <div className="bg-white flex flex-grow justify-center items-center flex-col rounded-lg">
-        {priceAlertLoading || updating ? (
+        {priceAlertLoading || loading ? (
           <div className="flex justify-center items-center w-full h-screen">
             <div className="animate-spin rounded-full h-12 w-12 border-t-4 border-b-4 border-blue-500"></div>
           </div>
