834e657a4adfcc54701a1c341a548f18ee3fde87
gimsangjun <5321913@naver.com>
2024-06-10 20:50:47 +0900

    refactor: 강화 계산기 최소 width 수정

--- src/pages/EnhancingCalPage.js
+++ src/pages/EnhancingCalPage.js
@@ -4,9 +4,9 @@ import EnCalContainer from "../containers/EnCalContainer";
 
 export default function EnhancingCalPage() {
   return (
-    <div className="flex min-w-[1080px] min-h-screen flex-col bg-gray-200">
+    <div className="flex min-w-[1350px] min-h-screen flex-col bg-gray-200">
       <HeaderContainer />
-      <EnCalContainer itemName={"데보레카 목걸이"} />
+      <EnCalContainer itemName={"데보레카 허리띠"} />
     </div>
   );
 }
