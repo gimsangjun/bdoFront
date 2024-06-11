2b601d3eb0d35b5ec8e99884f6c94df8e327daab
gimsangjun <5321913@naver.com>
2024-06-11 12:57:16 +0900

    refactor: min-width수정 및 주석 제거

--- src/components/item/TableHeader.js
+++ src/components/item/TableHeader.js
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 
-// TODO: 1. 이름을 일부만 입력해도 그 단어가 포함된 아이템들 리턴
-// TODO: 2. 자동완성기능  처럼 한글자씩 입력해도 그 이름을 가진 아이템들 리턴.
+// TODO: 자동완성기능  처럼 한글자씩 입력해도 그 이름을 가진 아이템들 리턴.
 export default function TableHeader({
   tableName,
   itemSearchValue = "",
@@ -30,10 +29,11 @@ export default function TableHeader({
         <h1 className="mr-auto whitespace-nowrap text-3xl font-bold div">
           {tableName}
         </h1>
-        <div className="relative flex items-center justify-center rounded-lg pr-2 bg-base-200 borde opacity-90 hover:opacity-100 h-[40px] py-0.5 pl-2 bg-white">
+
+        <div className="relative flex items-center justify-center rounded-lg pr-2 bg-base-200 borde opacity-90 hover:opacity-100 h-[40px] py-0.5 pl-2 bg-white mr-2">
           <input
             autoComplete="off"
-            className="border-none bg-transparent px-2 font-medium outline-none placeholder:text-opacity-70  placeholder-gray-400 text-base"
+            className="border-none bg-transparent px-2 font-medium outline-none placeholder:text-opacity-70 placeholder-gray-400 text-base"
             placeholder="아이템 이름"
             value={itemName}
             onChange={handleInputChange}
--- src/pages/EnhancingCalPage.js
+++ src/pages/EnhancingCalPage.js
@@ -4,7 +4,7 @@ import EnCalContainer from "../containers/EnCalContainer";
 
 export default function EnhancingCalPage() {
   return (
-    <div className="flex min-w-[1350px] min-h-screen flex-col bg-gray-200">
+    <div className="flex min-w-[1400px] min-h-screen flex-col bg-gray-200">
       <HeaderContainer />
       <EnCalContainer itemName={"데보레카 허리띠"} />
     </div>
