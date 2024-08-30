2d15b8775f6a5ea3b80eee1c80b2b76b94eda7f0
gimsangjun <5321913@naver.com>
2024-08-31 03:44:00 +0900

    refactor: 바뀐 백엔드에 맞춰서 api 요청 수정

--- src/utils/itemAPI.js
+++ src/utils/itemAPI.js
@@ -1,18 +1,34 @@
 import axios from "./axiosInstance";
 
 class ItemAPI {
+  //
   // 아이템
-  static async getItemsByQuery(query, page = 1) {
+  static async getItemsByQuery(queryParams, page = 1, limit = 10) {
     try {
-      const response = await axios.post(
-        `${process.env.REACT_APP_API_URL}/item/`,
-        {
-          query,
-          page,
-        },
+      // 서버 URL과 쿼리 파라미터 구성
+      const { id, sid, name, mainCategory } = queryParams;
+      const params = new URLSearchParams({
+        ...(id && { id }),
+        ...(sid && { sid }),
+        ...(name && { name }),
+        ...(mainCategory && { mainCategory }),
+        page,
+        limit,
+      });
+
+      const response = await axios.get(
+        `${process.env.REACT_APP_API_URL}/item/?${params.toString()}`,
       );
-      const { items, totalCount, pages, currentPage } = response.data;
-      return { status: response.status, items, totalCount, pages, currentPage };
+      const { items, totalItems, totalPages, currentPage } = response.data;
+      console.log("items:", items);
+
+      return {
+        status: response.status,
+        items,
+        totalCount: totalItems,
+        pages: totalPages,
+        currentPage,
+      };
     } catch (error) {
       console.error("getItemsByQuery Error:", error);
       throw error;
@@ -47,121 +63,6 @@ class ItemAPI {
       },
     );
     return { status: response.status, updateItems: response.data.items };
-  }
-
-  // 즐겨찾기
-  static async getFavItem() {
-    try {
-      const response = await axios.get(
-        `${process.env.REACT_APP_API_URL}/item/favorite`,
-      );
-      return { status: response.status, favItems: response.data.itemFavorites };
-    } catch (error) {
-      console.error("getItemFavorite Error", error);
-      throw error;
-    }
-  }
-
-  static async addFavItem(item) {
-    try {
-      const { id, sid } = item;
-      const response = await axios.post(
-        `${process.env.REACT_APP_API_URL}/item/favorite`,
-        {
-          id,
-          sid,
-        },
-      );
-      return { status: response.status };
-    } catch (error) {
-      console.error("addItemFavorite Error", error);
-      throw error;
-    }
-  }
-
-  static async removeFavItem(item) {
-    try {
-      const { id, sid } = item;
-      const response = await axios.delete(
-        `${process.env.REACT_APP_API_URL}/item/favorite?id=${id}&sid=${sid}`,
-      );
-      return { status: response.status };
-    } catch (error) {
-      console.error("remoeItemFavorite Error", error);
-      throw error;
-    }
-  }
-
-  // 가격 알림
-  static async addItemPriceAlert(item, priceThreshold, alertCondition) {
-    try {
-      const response = await axios.post(
-        `${process.env.REACT_APP_API_URL}/item/alert`,
-        {
-          itemName: item.name,
-          itemId: item.id,
-          itemSid: item.sid,
-          priceThreshold,
-          alertCondition, // alertCondition 추가
-        },
-      );
-      return {
-        status: response.status,
-        itemPriceAlert: response.data.itemPriceAlert,
-      };
-    } catch (error) {
-      console.error("addItemPriceAlert Error", error);
-      return {
-        status: error.response ? error.response.status : 500,
-        message: error.response ? error.response.data.message : "서버 오류",
-      };
-    }
-  }
-
-  static async getItemPriceAlerts() {
-    try {
-      const response = await axios.get(
-        `${process.env.REACT_APP_API_URL}/item/alert`,
-      );
-      return {
-        status: response.status,
-        itemPriceAlerts: response.data.itemPriceAlerts,
-      };
-    } catch (error) {
-      console.error("getItemPriceAlerts Error", error);
-      throw error;
-    }
-  }
-
-  static async updateItemPriceAlert(alertId, priceThreshold) {
-    try {
-      const response = await axios.put(
-        `${process.env.REACT_APP_API_URL}/item/alert`,
-        {
-          alertId,
-          priceThreshold,
-        },
-      );
-      return {
-        status: response.status,
-        itemPriceAlert: response.data.itemPriceAlert,
-      };
-    } catch (error) {
-      console.error("updateItemPriceAlert Error", error);
-      throw error;
-    }
-  }
-
-  static async deleteItemPriceAlert(alertId) {
-    try {
-      const response = await axios.delete(
-        `${process.env.REACT_APP_API_URL}/item/alert?id=${alertId}`,
-      );
-      return { status: response.status };
-    } catch (error) {
-      console.error("deleteItemPriceAlert Error", error);
-      throw error;
-    }
   }
 
   // 강화 정보 가져오기
